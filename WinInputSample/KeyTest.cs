07f15b69f5668f177b545ca29f8a813f38acc832
Byulbram <byulbram@ck.ac.kr>
2016-03-23 21:50:28 +0900

    Sample Scene for WinInput
    Sample Scene for WinInput

---
+++ WinInputSample/KeyTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using WindowsInput;
+
+public class KeyTest : MonoBehaviour {
+    
+	void Update () 
+    {
+        float X_delta = 0;
+        float Y_delta = 0;
+
+        if (WinInput.GetKey(KeyCode.W)) 
+        {
+            Y_delta += Time.deltaTime * 2;   
+        }
+
+        if (WinInput.GetKey(KeyCode.S)) 
+        {
+            Y_delta -= Time.deltaTime * 2;   
+        }
+
+        if (WinInput.GetKey(KeyCode.A))
+        {
+            X_delta -= Time.deltaTime * 2;   
+        }
+
+        if (WinInput.GetKey(KeyCode.D))
+        {
+            X_delta += Time.deltaTime * 2;   
+        }
+
+        transform.Translate(new Vector3(X_delta, Y_delta, 0));
+	}
+
+}
