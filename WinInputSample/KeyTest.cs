2d1081100250976840cec075ea48546217f15617
Byulbram" KwangSam Kim <byulbram@gmail.com>
2021-01-08 12:40:41 +0900

    Add Check for Key Down and Up
    키가 눌렸을때와 떼졌을때를 확인하는 변수를 추가합니다.
    Thanks to Flashscope
    
    GetKeyDown : 키가 눌렸을때
    GetKeyDown : 키가 떼졌을때

--- WinInputSample/KeyTest.cs
+++ WinInputSample/KeyTest.cs
@@ -3,30 +3,51 @@ using System.Collections;
 using WindowsInput;
 
 public class KeyTest : MonoBehaviour {
-    
-	void Update () 
+
+    public Material myColor;
+    int ColorCounter = 0;
+
+    void Update()
     {
         float X_delta = 0;
         float Y_delta = 0;
 
-        if (WinInput.GetKey(KeyCode.W)) 
+        if (WinInput.GetKey(KeyCode.W))
         {
-            Y_delta += Time.deltaTime * 2;   
+            Y_delta += Time.deltaTime * 2;
         }
 
-        if (WinInput.GetKey(KeyCode.S)) 
+        if (WinInput.GetKey(KeyCode.S))
         {
-            Y_delta -= Time.deltaTime * 2;   
+            Y_delta -= Time.deltaTime * 2;
         }
 
         if (WinInput.GetKey(KeyCode.A))
         {
-            X_delta -= Time.deltaTime * 2;   
+            X_delta -= Time.deltaTime * 2;
         }
 
         if (WinInput.GetKey(KeyCode.D))
         {
-            X_delta += Time.deltaTime * 2;   
+            X_delta += Time.deltaTime * 2;
+        }
+
+        //Check GetKeyDown by Color during 3 frame
+        if (ColorCounter <= 0)
+            myColor.color = Color.white;
+        else
+            ColorCounter--;
+
+        if (WinInput.GetKeyDown(KeyCode.W) || WinInput.GetKeyDown(KeyCode.A) || WinInput.GetKeyDown(KeyCode.S) || WinInput.GetKeyDown(KeyCode.D))
+        {
+            myColor.color = Color.red;
+            ColorCounter = 3;
+        }
+
+        if (WinInput.GetKeyUp(KeyCode.W) || WinInput.GetKeyUp(KeyCode.A) || WinInput.GetKeyUp(KeyCode.S) || WinInput.GetKeyUp(KeyCode.D))
+        {
+            myColor.color = Color.blue;
+            ColorCounter = 3;
         }
 
         transform.Translate(new Vector3(X_delta, Y_delta, 0));
